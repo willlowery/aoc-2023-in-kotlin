bf812f95a019b0d6a34cc0349e86b979eaadbaad
will.lowery <will.lowery@gopai.com>
2023-12-10 13:46:56 -0700

    Solution for day 08 part two.

--- src/Day08.kt
+++ src/Day08.kt
@@ -34,32 +34,51 @@ fun main() {
         return count
     }
 
-    fun part2(data: Pair<String, List<Node>>): Int {
+    fun part2(data: Pair<String, List<Node>>): Long {
         val instruction = data.first
         val network = data.second.fold(mutableMapOf<String, Node>()) { acc, node ->
             acc[node.id] = node
             acc
         }
 
-        var currentNode = data.second.filter { it.id.endsWith("A") }.first()
+        val currentNode = data.second.filter { it.id.endsWith("A") }.toMutableList()
 
-        (0..instruction.length * 5).fold(mutableListOf(currentNode)) { acc, i ->
-            val last = acc.last()
-            if (instruction[i.mod(instruction.length)] == 'L') {
-                acc.add(network[last.left]!!)
-            } else {
-                acc.add(network[last.right]!!)
+        val counts = currentNode.map {
+            var current = it
+            var count = 0
+            while (!current.id.endsWith("Z")) {
+                val direction = instruction[count.mod(instruction.length)]
+                current = if (direction == 'L') {
+                    network[current.left]!!
+                } else {
+                    network[current.right]!!
+                }
+                count++
             }
-            acc
-        }.forEach{ it.println()}
 
+            count
+        }
 
-        return 0
+        return counts.fold(counts.first().toLong()) { acc, i ->
+            lcm(acc, i.toLong())
+        }
     }
 
     val input = readInput("Day08")
-    part1(read(input)).println()
-//    part2(read(input)).println()
+    part1(read(input)).println() // 16897
+    part2(read(input)).println() // 16563603485021L
+}
+
+fun lcm(left: Long, right: Long): Long {
+    var gcd = 1
+    var i = 1
+    while (i <= left && i <= right) {
+        if (left % i == 0L && right % i == 0L)
+            gcd = i
+        ++i
+    }
+
+    return left * right / gcd
 }
 
 data class Node(val id: String, val left: String, val right: String)
